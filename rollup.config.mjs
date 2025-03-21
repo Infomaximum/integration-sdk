dd5d7ae673058a44f8de133bf83a1535864cde57
Alexandr Petyaykin <saneksa2@gmail.com>
2025-03-21 08:17:56 +0300

    fix: исправлено генерирование единого .d.ts файла

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import del from "rollup-plugin-delete";
+import { dts } from "rollup-plugin-dts";
 
 const externalPackages = [
   ...Object.keys(packageJson.dependencies || {}),
@@ -30,12 +31,17 @@ const config = [
       },
     ],
     plugins: [
+      del({ targets: "dist", hook: "buildStart", runOnce: true }),
       typescript(),
       resolve(),
       commonjs(),
-      del({ targets: "dist", hook: "buildStart", runOnce: true }),
     ],
     external: regexesOfPackages,
+  },
+  {
+    input: "dist/dts/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts(), del({ targets: "dist/dts", hook: "buildEnd", runOnce: true })],
   },
 ];
 
