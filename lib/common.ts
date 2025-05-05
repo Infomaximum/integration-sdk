f076784a68166c456a9f08fd14e9abbe0bfe2959
Егор Барашихин <miracles.on.the.turn@gmail.com>
2025-05-05 09:51:20 +0300

    fix: Убрана лишняя вложенность для ошибок и добавлен новый метод 'hoo… (#6)
    * fix: Убрана лишняя вложенность для ошибок и добавлен новый метод 'hook' для service
    
    * BREAKING CHANGE: изменения под новый серверный движок
    
    ---------
    
    Co-authored-by: Alexandr Petyaykin <saneksa2@gmail.com>

--- lib/common.ts
+++ lib/common.ts
@@ -23,20 +23,26 @@ export type MutatingRequestConfig = BaseRequestConfig & {
 
 export type RequestConfig = GetRequestConfig | MutatingRequestConfig;
 
-export type ExecuteServiceError = {
-  stringError(message: string): never;
-  cause(message: string): never;
-};
-
 export type RequestResult<Response = any> = {
   status: number;
   response: Readonly<Partial<Response>>;
 };
-
-export type ExecuteService = {
+export type ExecuteServiceAuth = {
+  hook: (
+    fn: (url: string, headers: Record<string, string>) => string,
+    guid: string,
+    timeout: number
+  ) => string | undefined;
+};
+export type ExecuteServiceUtils = {
   base64Encode: (input: string) => string;
   base64Decode: (input: string) => string;
-  request: <Response>(config: RequestConfig) => RequestResult<Response>;
-  error: ExecuteServiceError;
+  stringError(message: string): never;
   console: (input: string) => void;
 };
+
+export type ExecuteServiceNetwork = {
+  request: <Response>(config: RequestConfig) => RequestResult<Response>;
+};
+
+export type ExecuteService = ExecuteServiceAuth & ExecuteServiceNetwork & ExecuteServiceUtils;
